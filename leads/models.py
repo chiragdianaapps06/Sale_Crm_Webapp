03131b1029b136f93aafb9c6c40c139450e25ec4
lokesh <lokesh.saini@dianapps.com>
2025-08-11 17:03:31 +0530

    apps created for pipeline and leads

---
+++ leads/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from accounts.models import AbsModel
+
+class Leads(AbsModel):
+    title=models.CharField(max_length=100)
+    email=models.EmailField()
+    assigned_from=models.ForeignKey('accounts.CustomUser',on_delete=models.SET_NULL,null=True,related_name='leads_referrar')
+    assigned_to=models.ForeignKey('accounts.CustomUser',on_delete=models.CASCADE,related_name='leads_sales')
