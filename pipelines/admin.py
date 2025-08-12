41a7a6538cd63deb6f42daa5c9a51b3c487a614e
lokesh <lokesh.saini@dianapps.com>
2025-08-12 15:04:58 +0530

    changes into the admin site

--- pipelines/admin.py
+++ pipelines/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pipeline,PipelineStages
 
-# Register your models here.
+
+
+admin.site.register(Pipeline)
+
+admin.site.register(PipelineStages)
