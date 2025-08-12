bf7ee55c3f5c460ad298dcdbadd4750fb5b37179
lokesh <lokesh.saini@dianapps.com>
2025-08-12 09:58:01 +0530

    models created

--- leads/models.py
+++ leads/models.py
@@ -4,5 +4,7 @@ from accounts.models import AbsModel
 class Leads(AbsModel):
     title=models.CharField(max_length=100)
     email=models.EmailField()
+    description=models.TextField()
     assigned_from=models.ForeignKey('accounts.CustomUser',on_delete=models.SET_NULL,null=True,related_name='leads_referrar')
     assigned_to=models.ForeignKey('accounts.CustomUser',on_delete=models.CASCADE,related_name='leads_sales')
+    pipeline=models.ForeignKey('pipelines.Pipeline',on_delete=models.SET_NULL,null=True)
