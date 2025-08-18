4396f273e56456649874b6de9c116fe678a20f34
lokesh <lokesh.saini@dianapps.com>
2025-08-18 14:13:48 +0530

    password issue resolved for sale user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+
 CustomUser = get_user_model()
-# Register your models here.
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'brand', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+
+admin.site.register(CustomUser,CustomUserAdmin)
 
