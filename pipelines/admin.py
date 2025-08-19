d953a4585b0367d0c94c73df5ad948284cd81d41
lokesh <lokesh.saini@dianapps.com>
2025-08-19 18:06:21 +0530

    pipeline implemented

--- pipelines/admin.py
+++ pipelines/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import Pipeline
+from .models import Pipeline,PipelineStatus
+
+class PipelineAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
+    # list_filter = ['status', 'assigned_to', 'assigned_from']
+    # list_filter = ['status']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        elif request.user.user_type == 'sale':
+            return qs.filter(user=request.user)
+        elif request.user.user_type == 'ref':
+            return qs.filter(user=request.user)
+        return qs.none()
 
 
-
-admin.site.register(Pipeline)
+admin.site.register(Pipeline,PipelineAdmin)
+admin.site.register(PipelineStatus)
 
 
--- pipelines/models.py
+++ pipelines/models.py
@@ -3,7 +3,6 @@ from accounts.models import AbsModel
 from .choices import PipelineStages
 
 #actual pipeline model
-
 class Pipeline(AbsModel):
     name=models.CharField(max_length=100)
     user=models.ForeignKey('accounts.CustomUser',on_delete=models.CASCADE)
@@ -13,7 +12,7 @@ class Pipeline(AbsModel):
 
 class PipelineStatus(AbsModel):
     pipeline_name=models.ForeignKey(Pipeline,on_delete=models.CASCADE)
-    stage=models.CharField(max_length=100,choices=PipelineStages.choices,default=PipelineStages.closed,null=True)
+    stage=models.CharField(max_length=100,choices=PipelineStages.choices,default=PipelineStages.new,null=True)
 
     def __str__(self):
         return f"{self.stage}"
